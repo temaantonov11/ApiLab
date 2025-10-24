2e58a7dd08020c4aaa4a4957d87c71afb9cf1109
temaantonov11 <temaantonov11@yandex.ru>
2025-10-25 01:52:04 +0300

    added multipages build in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/ApiLab/"
+    base: "/ApiLab/",
+    build: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            weather: resolve(__dirname, 'weather.html'),
+        }
+    }
 })
